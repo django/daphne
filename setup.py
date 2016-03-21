a5946922f5295094d8315569414cffab6b0d2488
Andrew Godwin <andrew@aeracode.org>
2016-03-21 16:45:24 -0700

    Releasing version 0.10

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'asgiref>=0.9',
+        'asgiref>=0.10',
         'twisted>=15.5',
         'autobahn>=0.12',
     ],
