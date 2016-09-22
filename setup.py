5a5fd08633d343506a69600fbae51ec9ca1b837d
Adam Chainz <adam@adamj.eu>
2016-09-22 22:25:09 +0100

    Tidy up setup.py a bit
    * Remove unused import 'sys'
    * Github is HTTPS
    * Add some trove classifiers based upon Django's

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 import os
-import sys
+
 from setuptools import find_packages, setup
+
 from daphne import __version__
 
 
 # We use the README as the long_description
 readme_path = os.path.join(os.path.dirname(__file__), "README.rst")
-
+with open(readme_path) as fp:
+    long_description = fp.read()
 
 setup(
     name='daphne',
     version=__version__,
-    url='http://github.com/django/daphne',
+    url='https://github.com/django/daphne',
     author='Django Software Foundation',
     author_email='foundation@djangoproject.com',
     description='Django ASGI (HTTP/WebSocket) server',
-    long_description=open(readme_path).read(),
+    long_description=long_description,
     license='BSD',
     zip_safe=False,
     packages=find_packages(),
@@ -28,4 +30,17 @@ setup(
     entry_points={'console_scripts': [
         'daphne = daphne.cli:CommandLineInterface.entrypoint',
     ]},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
 )
