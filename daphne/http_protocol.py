0a2c6c2ff230a5d65706de5a3cc8e2670cf9d71c
Andrew Godwin <andrew@aeracode.org>
2018-02-21 09:50:59 -0800

    Fix #175: Check finished as well as channel

--- daphne/http_protocol.py
+++ daphne/http_protocol.py
@@ -190,7 +190,7 @@ class WebRequest(http.Request):
         Handles a reply from the client
         """
         # Handle connections that are already closed
-        if self.channel is None:
+        if self.finished or self.channel is None:
             return
         # Check message validity
         if "type" not in message:
