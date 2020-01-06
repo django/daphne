196cfaffbef7d6a1065d7bed26239376852f019b
Brishen Hawkins <brishen.hawkins@gmail.com>
2020-01-06 15:50:57 -0700

    Fix formatting

--- daphne/server.py
+++ daphne/server.py
@@ -1,6 +1,6 @@
 # This has to be done first as Twisted is import-order-sensitive with reactors
 import asyncio  # isort:skip
-import selectors # isort:skip
+import selectors  # isort:skip
 import sys  # isort:skip
 import warnings  # isort:skip
 from twisted.internet import asyncioreactor  # isort:skip
