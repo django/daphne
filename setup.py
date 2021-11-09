77196c1732e31c854bc1a11b4b4a82e285597d7b
Carlton Gibson <carlton.gibson@noumenal.es>
2021-11-09 15:37:04 +0100

    Added Python 3.10 to CI.

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Internet :: WWW/HTTP",
     ],
 )
