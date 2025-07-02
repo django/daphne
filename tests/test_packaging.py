django/daphne

commit 630caed9153f8eb62aecfade3e349d3cc6ee1bb0

Upgrade project metadata (#542)

pyproject.toml
@@ -1,3 +1,81 @@
+[project]
+name = "daphne"
+dynamic = ["version"]
+description = "Django ASGI (HTTP/WebSocket) server"
+requires-python = ">=3.9"
+authors = [
+    { name = "Django Software Foundation", email = "foundation@djangoproject.com" },
+]
+
+license = { text = "BSD" }
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Environment :: Web Environment",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
+    "Topic :: Internet :: WWW/HTTP",
+]
+
+dependencies = ["asgiref>=3.5.2,<4", "autobahn>=22.4.2", "twisted[tls]>=22.4"]
+
+[project.optional-dependencies]
+tests = [
+    "django",
+    "hypothesis",
+    "pytest",
+    "pytest-asyncio",
+    "pytest-cov",
+    "black",
+    "tox",
+    "flake8",
+    "flake8-bugbear",
+    "mypy",
+]
+
+[project.urls]
+homepage = "https://github.com/django/daphne"
+documentation = "https://channels.readthedocs.io"
+repository = "https://github.com/django/daphne.git"
+changelog = "https://github.com/django/daphne/blob/main/CHANGELOG.txt"
+issues = "https://github.com/django/daphne/issues"
+
+[project.scripts]
+daphne = "daphne.cli:CommandLineInterface.entrypoint"
+
 [build-system]
 requires = ["setuptools"]
 build-backend = "setuptools.build_meta"
+
+[tool.setuptools]
+packages = ["daphne"]
+
+[tool.setuptools.dynamic]
+version = { attr = "daphne.__version__" }
+readme = { file = "README.rst", content-type = "text/x-rst" }
+
+[tool.isort]
+profile = "black"
+
+[tool.pytest]
+testpaths = ["tests"]
+asyncio_mode = "strict"
+filterwarnings = ["ignore::pytest.PytestDeprecationWarning"]
+
+[tool.coverage.run]
+omit = ["tests/*"]
+concurrency = ["multiprocessing"]
+
+[tool.coverage.report]
+show_missing = "true"
+skip_covered = "true"
+
+[tool.coverage.html]
+directory = "reports/coverage_html_report"

.flake8
@@ -1,0 +1,11 @@
+[flake8]
+exclude =
+    .venv,
+    .tox,
+    docs,
+    testproject,
+    js_client,
+    .eggs
+
+extend-ignore = E123, E128, E266, E402, W503, E731, W601, B036
+max-line-length = 120

.github/workflows/tests.yml
@@ -5,6 +5,10 @@ on:
     branches:
       - main
   pull_request:
+  workflow_dispatch:
+
+permissions:
+  contents: read
 
 jobs:
   tests:

.gitignore
@@ -13,3 +13,4 @@ test_consumer*
 .python-version
 .pytest_cache/
 .vscode
+.coverage

daphne/testing.py
@@ -158,7 +158,7 @@ class DaphneProcess(multiprocessing.Process):
                 application=application,
                 endpoints=endpoints,
                 signal_handlers=False,
-                **self.kwargs
+                **self.kwargs,
             )
             # Set up a poller to look for the port
             reactor.callLater(0.1, self.resolve_port)

setup.cfg
@@ -1,59 +1,0 @@
-[metadata]
-name = daphne
-version = attr: daphne.__version__
-url = https://github.com/django/daphne
-author = Django Software Foundation
-author_email = foundation@djangoproject.com
-description = Django ASGI (HTTP/WebSocket) server
-long_description = file: README.rst
-long_description_content_type = text/x-rst
-license = BSD
-classifiers =
-    Development Status :: 4 - Beta
-    Environment :: Web Environment
-    Intended Audience :: Developers
-    License :: OSI Approved :: BSD License
-    Operating System :: OS Independent
-    Programming Language :: Python
-    Programming Language :: Python :: 3
-    Programming Language :: Python :: 3.9
-    Programming Language :: Python :: 3.10
-    Programming Language :: Python :: 3.11
-    Programming Language :: Python :: 3.12
-    Programming Language :: Python :: 3.13
-    Topic :: Internet :: WWW/HTTP
-
-[options]
-package_dir =
-    daphne=daphne
-    twisted=daphne/twisted
-include_package_data = True
-install_requires =
-    asgiref>=3.5.2,<4
-    autobahn>=22.4.2
-    twisted[tls]>=22.4
-python_requires = >=3.9
-zip_safe = False
-
-[options.entry_points]
-console_scripts =
-    daphne = daphne.cli:CommandLineInterface.entrypoint
-
-[options.extras_require]
-tests =
-    django
-    hypothesis
-    pytest
-    pytest-asyncio
-
-[flake8]
-exclude = venv/*,tox/*,docs/*,testproject/*,js_client/*,.eggs/*
-extend-ignore = E123, E128, E266, E402, W503, E731, W601, B036
-max-line-length = 120
-
-[isort]
-profile = black
-
-[tool:pytest]
-testpaths = tests
-asyncio_mode = strict

commit aa2dee2cf93d7500c5f246d3adc171b05acce000

Fix twisted plugin installation with new packaging. (#562)
* Added test for twisted plugin installation.

Refs #557.

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* Fixed setuptools configuration

Port correct configuration from old setup.cfg file.

Regression in #542. Fixes #557.

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

pyproject.toml
@@ -55,7 +55,7 @@ requires = ["setuptools"]
 build-backend = "setuptools.build_meta"
 
 [tool.setuptools]
-packages = ["daphne"]
+package-dir = { daphne = "daphne", twisted = "daphne/twisted" }
 
 [tool.setuptools.dynamic]
 version = { attr = "daphne.__version__" }

tests/test_packaging.py
@@ -1,0 +1,15 @@
+import sys
+from pathlib import Path
+
+
+def test_fd_endpoint_plugin_installed():
+    # Find the site-packages directory
+    for path in sys.path:
+        if "site-packages" in path:
+            site_packages = Path(path)
+            break
+    else:
+        raise AssertionError("Could not find site-packages in sys.path")
+
+    plugin_path = site_packages / "twisted" / "plugins" / "fd_endpoint.py"
+    assert plugin_path.exists(), f"fd_endpoint.py not found at {plugin_path}"